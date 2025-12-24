0a6e7881a9c8b961a386c98b2d087746b94e28ea
dennisj-sam <Dennisj_50_90@hotmail.com>
2025-12-24 04:47:58 +0000

    Reemplazo contenido con código real de las soluciones

--- solucion_c.py
+++ solucion_c.py
@@ -2,29 +2,59 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+# Cargar datos
 url = "https://github.com/robintux/Datasets4StackOverFlowQuestions/raw/master/Cancer_Pulmon.zip"
 df = pd.read_csv(url, compression='zip')
 
+# Crear figura
 fig, axes = plt.subplots(2, 2, figsize=(20, 12))
 fig.suptitle('Cancer', fontsize=16, fontweight='bold')
+
+
+# Histograma de la columna/variable: age
 
 axes[0, 0].hist(df['age'], bins=5, color='skyblue', edgecolor='black')
 axes[0, 0].set_title('Distribucion de la edad')
 axes[0, 0].set_xlabel('Edad')
 axes[0, 0].set_ylabel('Frecuencia')
 
+
+# Diagrama de tipo de pie de la variable/columna: gender
+
 gender_counts = df['gender'].value_counts()
-axes[0, 1].pie(gender_counts, labels=gender_counts.index, autopct='%1.1f%%', startangle=90)
+
+axes[0, 1].pie(
+    gender_counts,
+    labels=gender_counts.index,
+    autopct='%1.1f%%',
+    startangle=90
+)
 axes[0, 1].set_title('Distribucion del genero')
 
+
+# Distribución de paises (Columan: country) en un diagrama de barras
+
 country_counts = df['country'].value_counts()
-axes[1, 0].bar(country_counts.index, country_counts.values, color='lightcoral')
+
+axes[1, 0].bar(
+    country_counts.index,
+    country_counts.values,
+    color='lightcoral'
+)
 axes[1, 0].set_title('Pacientes por pais')
 axes[1, 0].set_ylabel('Numero de pacientes')
 axes[1, 0].tick_params(axis='x', rotation=90)
 
+
+# Distribución de la etapa del cáncer (columna cancer_stage) en una diagrama de barras.
+
 stage_counts = df['cancer_stage'].value_counts()
-axes[1, 1].bar(stage_counts.index, stage_counts.values, color='lightgreen')
+
+axes[1, 1].bar(
+    stage_counts.index,
+    stage_counts.values,
+    color='lightgreen'
+)
 axes[1, 1].set_title('Distribucion de la etapa del cancer')
 axes[1, 1].set_ylabel('Numero de observaciones')
 
--- solucion_d.py
+++ solucion_d.py
@@ -4,13 +4,22 @@ import matplotlib.pyplot as plt
 fig, axes = plt.subplots(1, 3, figsize=(18, 6))
 fig.suptitle('Boxplots de variables age, BMI y cholesterol_level', fontsize=16, fontweight='bold')
 
+
+# Boxplot de edad
+
 axes[0].boxplot(df['age'], vert=True)
 axes[0].set_title('Age')
 axes[0].set_ylabel('Edad')
 
+
+# Boxplot de BMI
+
 axes[1].boxplot(df['bmi'], vert=True)
 axes[1].set_title('BMI')
 axes[1].set_ylabel('Índice de Masa Corporal')
+
+
+# Boxplot de colesterol
 
 axes[2].boxplot(df['cholesterol_level'], vert=True)
 axes[2].set_title('Cholesterol Level')
